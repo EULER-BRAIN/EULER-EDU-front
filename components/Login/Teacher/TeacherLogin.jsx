93ef971753578dd3558c2af499702571f1baa4c8
14Kgun <geon6757@kaist.ac.kr>
2022-06-28 00:07:01 +0900

    Update TeacherLogin.jsx

--- components/Login/Teacher/TeacherLogin.jsx
+++ components/Login/Teacher/TeacherLogin.jsx
@@ -2,9 +2,32 @@ import HeaderEmpty from '@components/Header/HeaderEmpty'
 import Footer from '@components/Footer/Footer'
 
 const TeacherLogin = () => {
+  const styleEuler = {
+    textAlign: 'center',
+    color: '#c2151c',
+    fontSize: '26px'
+  }
+  const styleTitle = {
+    textAlign: 'center',
+    color: 'rgb(60,60,60)',
+    fontSize: '20px'
+  }
   return (
     <div>
       <HeaderEmpty />
+      <div style={{
+        marginLeft: '20px',
+        marginRight: '20px'
+      }}>
+        <div style={{
+          maxWidth: '280px',
+          margin: 'auto'
+        }}>
+          <div style={{ height: '40px' }}/>
+          <div style={ styleEuler } className="FBold">EULER</div>
+          <div style={ styleTitle } className="FBold">선생님 로그인</div>
+        </div>
+      </div>
       <Footer />
     </div>
   )
