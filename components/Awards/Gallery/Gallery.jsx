a71525ee4895884ca6b5f787f2116a821d3dfe8e
14Kgun <geon6757@kaist.ac.kr>
2022-06-28 16:34:06 +0900

    Add Awards/Gallery

--- components/Awards/Gallery/Gallery.jsx
+++ components/Awards/Gallery/Gallery.jsx
@@ -1,14 +1,48 @@
+import AwesomeSlider from 'react-awesome-slider';
 import HeaderEmpty from "@components/Header/HeaderEmpty"
 
-const Gallery = () => {
+import 'react-awesome-slider/dist/styles.css';
+
+const Award = () => {
+  const style = {
+    position: 'absolute',
+    top: '0px', left: '0px',
+    width: '100%', height: '100%',
+    background: '#f3f3f3'
+  }
+  const styleBody = {
+    width: '100%',
+    height: 'calc(100% - 100px)',
+    position: 'relative'
+  }
   const styleBottom = {
     position: 'absolute',
     bottom: '0px', left: '0px',
     width: '100%',
     height: '100px',
     borderTop: '1px solid rgb(206,206,206)',
-    background: 'white'
+    background: 'white',
+    overflow: 'hidden'
   }
+  const styleContent = {
+    paddingLeft: '10px',
+    paddingRight: '10px',
+    paddingTop: '10px'
+  }
+  return (
+    <div style={ style }>
+      <div style={ styleBody }>
+        image
+      </div>
+      <div style={ styleBottom }>
+        <div style={ styleContent }>
+          Bottom - Content
+        </div>
+      </div>
+    </div>
+  )
+}
+const Gallery = () => {
   return (
     <div style={{
       position: 'fixed',
@@ -17,8 +51,19 @@ const Gallery = () => {
       overflow: 'hiddne'
     }}>
       <HeaderEmpty />
-      <div style={ styleBottom }>
-        123
+      <div style={{
+        position: 'absolute',
+        top: '50px', bottom: '0px',
+        left: '0px', right: '0px',
+      }}>
+        <AwesomeSlider
+          fillParent={ true }
+          infinite={ false }
+        >
+          <div><Award /></div>
+          <div><Award /></div>
+          <div><Award /></div>
+        </AwesomeSlider>
       </div>
     </div>
   )
