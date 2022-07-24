02e5e34f3df5009dcffec6b68d6a480c87d22635
14Kgun <geon6757@kaist.ac.kr>
2022-07-24 19:47:10 +0900

    Add /Management/Main/Award/AwardEdit.jsx

--- components/Management/Main/Award/AwardEdit.jsx
+++ components/Management/Main/Award/AwardEdit.jsx
@@ -1,15 +1,102 @@
+import { useEffect, useState, useRef } from "react";
+import Image from "next/image";
+import { useRouter } from "next/router";
 import LoadingDiv from "@components/Layout/Loading";
-import { Content, Title } from "@components/ParentSystem/Layout/LSet";
+import { Content, Title, TopBackLay, TopFlexBtn, TopFlexSaved, TopFlexText, TopInput } from "@components/ParentSystem/Layout/LSet";
 import axiosEDU from "@tools/axiosEDU";
-import { useEffect, useState } from "react";
+import { date2Str } from "@tools/trans";
+import getS3ImgUrl from "@tools/getS3ImgUrl";
+import regExpTest from "@tools/regExpTest";
+
+const S3Image = (props) => {
+  console.log(props.exist);
+  const styleLayDBtm = {
+    paddingTop: '5px',
+    display: 'flex',
+    justifyContent: 'right',
+    gap: '5px'
+  }
+
+  const router = useRouter();
+  const onCall = useRef(null);
+  const onDelete = () => {
+    if (!onCall.current) {
+      onCall.current = true;
+      axiosEDU.get(`/management/main/award/img/delete/${ props.id }`).then(({ data }) => {
+        onCall.current = false;
+        if (data.award) {
+          alert('이미지가 성공적으로 삭제되었습니다')
+          router.reload();
+        }
+        else {
+          alert('Permission denied : 요청 거부됨')
+        }
+      })
+    }
+  }
+
+  return (
+    <div>
+      {
+        props.exist ? (
+          <div style={{
+            width: '200px',
+            height: '200px',
+            background: 'rgb(200,200,200)'
+          }}>
+            <Image
+              src={ getS3ImgUrl(`awards/${ props.id }.png`) }
+              alt={ `awards/${ props.id }` }
+              width={ 200 }
+              height={ 200 }
+            />
+          </div>
+        ) : (
+          <div style={{
+            fontSize: '14px',
+            color: 'gray'
+          }}>
+            S3에 "{ props.id }"에 해댱하는 이미지가 없습니다.
+          </div>
+        )
+      }
+      {
+        props.exist ? (
+          <div style={ styleLayDBtm }>
+            <TopFlexBtn
+              onClick={ onDelete }
+            >
+              이미지 삭제
+            </TopFlexBtn>
+          </div>
+        ) : (
+          <div style={ styleLayDBtm }>
+            <TopFlexBtn
+              onClick={ onDelete }
+            >
+              이미지 추가
+            </TopFlexBtn>
+          </div>
+        )
+      }
+    </div>
+    
+  )
+}
 
 const AwardEdit = (props) => {
   const [award, setAward] = useState(null);
+  const [inputValue, setInputValue] = useState();
 
   useEffect(() => {
     axiosEDU.get(`/management/main/award/info/${ props.id }`).then(({ data }) => {
       if (data.award) {
-        setAward(data.award);
+        const award = { 
+          ...data.award,
+          imgFound: data.imgFound
+        };
+        setAward(award);
+        setInputValue(award);
       } 
       else {
         // FIXME
@@ -17,14 +104,199 @@ const AwardEdit = (props) => {
     });
   }, [props.id]);
 
+  const styleLayD = {
+    borderBottom: '1px solid rgb(206,206,206)',
+    padding: '10px',
+  }
+  const styleLayDTop = {
+    paddingBottom: '5px',
+    display: 'flex',
+    gap: '5px'
+  }
+  const styleLayDBtm = {
+    paddingTop: '5px',
+    display: 'flex',
+    justifyContent: 'right',
+    gap: '5px'
+  }
+
+  const router = useRouter();
+  const onCall = useRef();
+  const onClickName = () => {
+    const name = inputValue.name;
+    if (!regExpTest.awardName(name)) {
+      return alert(regExpTest.awardName.toString()+' 을 만족해야 합니다')
+    }
+    if (!onCall.current) {
+      onCall.current = true;
+      axiosEDU.post('/management/main/award/edit/name', {
+        id: props.id,
+        name: name
+      }).then(({ data }) => {
+        onCall.current = false;
+        if (data.award) {
+          setAward(data.award);
+        } else {
+          alert('Permission denied : 요청 거부됨')
+        }
+      })
+    }
+  }
+  const onClickContent = () => {
+    const content = inputValue.content;
+    if (!regExpTest.awardContent(content)) {
+      return alert(regExpTest.awardContent.toString()+' 을 만족해야 합니다')
+    }
+    if (!onCall.current) {
+      onCall.current = true;
+      axiosEDU.post('/management/main/award/edit/content', {
+        id: props.id,
+        content: content
+      }).then(({ data }) => {
+        onCall.current = false;
+        if (data.award) {
+          setAward(data.award);
+        } else {
+          alert('Permission denied : 요청 거부됨')
+        }
+      })
+    }
+  }
+  const onClickIsShow = (isShow) => () => {
+    if (!onCall.current) {
+      onCall.current = true;
+      axiosEDU.post('/management/main/award/edit/isShow', {
+        id: props.id,
+        isShow: isShow
+      }).then(({ data }) => {
+        onCall.current = false;
+        if (data.award) {
+          setAward(data.award);
+        } else {
+          alert('Permission denied : 요청 거부됨')
+        }
+      })
+    }
+  }
+  const onClickDelete = () => {
+    if (!onCall.current) {
+      onCall.current = true;
+      axiosEDU.get(`/management/main/award/delete/${ props.id }`).then(({ data }) => {
+        onCall.current = false;
+        if (data.result) {
+          alert('해당 어워드가 삭제되었습니다')
+          router.replace('/management/main/award');
+        }
+        else {
+          alert('Permission denied : 요청 거부됨')
+        }
+      })
+    }
+  }
+
   return (
     <div>
-      <Title>선생님 수정</Title>
+      <Title>어워드 수정</Title>
       <Content>
         {
           award ? (
             <div>
-              { award._id }
+              <TopBackLay />
+              <div style={ styleLayD }>
+                <div style={ styleLayDTop }>
+                  <TopFlexText>ID</TopFlexText>
+                </div>
+                <TopInput
+                  value={ award._id }
+                />
+              </div>
+              <div style={ styleLayD }>
+                <div style={ styleLayDTop }>
+                  <TopFlexText>제목</TopFlexText>
+                  <TopFlexSaved token={ award.name === inputValue.name } />
+                </div>
+                <TopInput
+                  value={ inputValue.name }
+                  onChange={ (x) => {
+                    if (RegExp("^.{0,30}$").test(x)) {
+                      setInputValue({
+                        ...inputValue,
+                        name: x
+                      })
+                    }
+                  } }
+                />
+                <div style={ styleLayDBtm }>
+                  <TopFlexBtn
+                    onClick={ onClickName }
+                  >제목 수정</TopFlexBtn>
+                </div>
+              </div>
+              <div style={ styleLayD }>
+                <div style={ styleLayDTop }>
+                  <TopFlexText>내용</TopFlexText>
+                  <TopFlexSaved token={ award.content === inputValue.content } />
+                </div>
+                <TopInput
+                  value={ inputValue.content }
+                  onChange={ (x) => {
+                    if (RegExp("^.{0,50}$").test(x)) {
+                      setInputValue({
+                        ...inputValue,
+                        content: x
+                      })
+                    }
+                  } }
+                />
+                <div style={ styleLayDBtm }>
+                  <TopFlexBtn
+                    onClick={ onClickContent }
+                  >내용 수정</TopFlexBtn>
+                </div>
+              </div>
+              <div style={ styleLayD }>
+                <div style={ styleLayDTop }>
+                  <TopFlexText>공개 여부</TopFlexText>
+                </div>
+                <TopInput
+                  value={ award.isShow ? '공개' : '비공개' }
+                />
+                <div style={ styleLayDBtm }>
+                  <TopFlexBtn
+                    onClick={ onClickIsShow(true) }
+                  >공개로 수정</TopFlexBtn>
+                  <TopFlexBtn
+                    onClick={ onClickIsShow(false) }
+                  >비공개로 수정</TopFlexBtn>
+                </div>
+              </div>
+              <div style={ styleLayD }>
+                <div style={ styleLayDTop }>
+                  <TopFlexText>등록 날짜</TopFlexText>
+                </div>
+                <TopInput
+                  value={ date2Str(award.registDate) }
+                />
+              </div>
+              <div style={ styleLayD }>
+                <div style={ styleLayDTop }>
+                  <TopFlexText>S3 이미지</TopFlexText>
+                </div>
+                <S3Image
+                  id={ award._id }
+                  exist={ award.imgFound }
+                />
+              </div>
+              <div style={ styleLayD }>
+                <div style={ styleLayDTop }>
+                  <TopFlexText>어워드 삭제</TopFlexText>
+                </div>
+                <div style={ styleLayDBtm }>
+                  <TopFlexBtn
+                    onClick={ onClickDelete }
+                  >어워드 삭제</TopFlexBtn>
+                </div>
+              </div>
             </div>
           ) : (
             <div>
--- tools/regExpTest.js
+++ tools/regExpTest.js
@@ -2,5 +2,7 @@ const loginId = (x) => RegExp("^[a-z0-9_-]{5,20}$").test(x);
 const loginPw = (x) => RegExp("^.{10,30}$").test(x);
 const name = (x) => RegExp("^[A-Za-z가-힣ㄱ-ㅎㅏ-ㅣ0-9-_ ]{2,15}$").test(x);
 const noticeTitle = (x) => RegExp("^.{1,40}$").test(x);
+const awardName = (x) => RegExp("^.{1,30}$").test(x);
+const awardContent = (x) => RegExp("^.{1,50}$").test(x);
 
-export default { loginId, loginPw, name, noticeTitle };
+export default { loginId, loginPw, name, noticeTitle, awardName, awardContent };
